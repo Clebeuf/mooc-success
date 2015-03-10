d1eb62e4ff1ba700c1098aaf6b948619c2c99fb5
Carly Lebeuf <clebeuf@uvic.ca>
2015-03-10 15:09:24 -0700

    Data parser is now worinking.  Excepts csv file as command line argument

--- parser.py
+++ parser.py
@@ -3,6 +3,7 @@ import sys
 import re
 import string
 import json
+from decimal import *
 
 #----------------------------------------------------------------------------------------------
 # Create an array of variables for each record
@@ -33,27 +34,36 @@ def filterRecords(records):
 
     for record in records:
 
-        if 'other' in str.lower(record['final_cc_cname_DI']):
-            record['final_cc_cname_DI'] = 'unknown'
-        
-        courseInfo = re.split('[/]',record['course_id'])
-        record['school'] = courseInfo[0]
-        record['course_id'] = courseInfo[1]
-        record['year'] = courseInfo[2]
+        # remove incomplete records & convert into proper types
+        if record['YoB'].isdigit() and record['grade'].isdigit() and record['ndays_act'].isdigit() and record['nplay_video'].isdigit() and record['nchapters'].isdigit() and record['nforum_posts'].isdigit():
+            record['YoB'] = int(record['YoB'])
+            record['grade'] = float(record['grade'])
+            record['nevents'] = int(record['nevents'])
+            record['ndays_act'] = int(record['ndays_act'])
+            record['nplay_video'] = int(record['nplay_video'])
+            record['nchapters'] = int(record['nchapters'])
+            record['nforum_posts'] = int(record['nforum_posts'])
 
+            # If the location is other convert it to unknown
+            if 'other' in str.lower(record['final_cc_cname_DI']):
+                record['final_cc_cname_DI'] = 'unknown'
+            
+            # split the course info into the school, year, and course ID
+            courseInfo = re.split('[/]',record['course_id'])
+            record['school'] = courseInfo[0]
+            record['course_id'] = courseInfo[1]
+            record['year'] = courseInfo[2]
 
-        record['viewed'] = int(record['viewed'])
-        record['explored'] = int(record['explored'])
-        record['certified'] = int(record['certified'])
-        # record['YoB'] = int(record['YoB'])
-        # record['grade'] = float(record['grade'])*100
-        # record['nevents'] = int(record['nevents'])
-        # record['ndays_act'] = int(record['ndays_act'])
-        # record['nplay_video'] = int(record['nplay_video'])
-        # record['nchapters'] = int(record['nchapters'])
-        # record['nforum_posts'] = int(record['nforum_posts'])
+            # find the year
+            courseYear = re.split('[_]',record['year'])
+            record['year'] = int(courseYear[0])
 
-        filteredRecords.append(record)
+            # convert into integer values
+            record['viewed'] = int(record['viewed'])
+            record['explored'] = int(record['explored'])
+            record['certified'] = int(record['certified'])
+
+            filteredRecords.append(record)
 
 
     return filteredRecords
@@ -93,9 +103,8 @@ def main():
     formattedRecords = formatRecords(records)
     
     filteredRecords = filterRecords(formattedRecords)
-
+    
     print json.dumps(filteredRecords)
-
 
 
 if __name__ == '__main__':
