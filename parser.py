8d40cd80a78422e063d805b7b0e044ec15e4bd4f
Carly Lebeuf <clebeuf@uvic.ca>
2015-03-12 12:55:25 -0700

    added an if statement to allow us to specify how many records we want outputted

--- parser.py
+++ parser.py
@@ -32,38 +32,46 @@ def formatRecords(records):
 def filterRecords(records):
     filteredRecords = []
 
+    counter = 0;
+
+    
     for record in records:
 
-        # remove incomplete records & convert into proper types
-        if record['YoB'].isdigit() and record['grade'].isdigit() and record['ndays_act'].isdigit() and record['nplay_video'].isdigit() and record['nchapters'].isdigit() and record['nforum_posts'].isdigit():
-            record['YoB'] = int(record['YoB'])
-            record['grade'] = float(record['grade'])
-            record['nevents'] = int(record['nevents'])
-            record['ndays_act'] = int(record['ndays_act'])
-            record['nplay_video'] = int(record['nplay_video'])
-            record['nchapters'] = int(record['nchapters'])
-            record['nforum_posts'] = int(record['nforum_posts'])
+        # limit data in the output file (very hacky)
+        if counter < 1000000:
 
-            # If the location is other convert it to unknown
-            if 'other' in str.lower(record['final_cc_cname_DI']):
-                record['final_cc_cname_DI'] = 'unknown'
-            
-            # split the course info into the school, year, and course ID
-            courseInfo = re.split('[/]',record['course_id'])
-            record['school'] = courseInfo[0]
-            record['course_id'] = courseInfo[1]
-            record['year'] = courseInfo[2]
+            # remove incomplete records & convert into proper types
+            if record['YoB'].isdigit() and record['grade'].isdigit() and record['ndays_act'].isdigit() and record['nplay_video'].isdigit() and record['nchapters'].isdigit() and record['nforum_posts'].isdigit():
+                record['YoB'] = int(record['YoB'])
+                record['grade'] = float(record['grade'])
+                record['nevents'] = int(record['nevents'])
+                record['ndays_act'] = int(record['ndays_act'])
+                record['nplay_video'] = int(record['nplay_video'])
+                record['nchapters'] = int(record['nchapters'])
+                record['nforum_posts'] = int(record['nforum_posts'])
 
-            # find the year
-            courseYear = re.split('[_]',record['year'])
-            record['year'] = int(courseYear[0])
+                # If the location is other convert it to unknown
+                if 'other' in str.lower(record['final_cc_cname_DI']):
+                    record['final_cc_cname_DI'] = 'unknown'
+                
+                # split the course info into the school, year, and course ID
+                courseInfo = re.split('[/]',record['course_id'])
+                record['school'] = courseInfo[0]
+                record['course_id'] = courseInfo[1]
+                record['year'] = courseInfo[2]
 
-            # convert into integer values
-            record['viewed'] = int(record['viewed'])
-            record['explored'] = int(record['explored'])
-            record['certified'] = int(record['certified'])
+                # find the year
+                courseYear = re.split('[_]',record['year'])
+                record['year'] = int(courseYear[0])
 
-            filteredRecords.append(record)
+                # convert into integer values
+                record['viewed'] = int(record['viewed'])
+                record['explored'] = int(record['explored'])
+                record['certified'] = int(record['certified'])
+
+                filteredRecords.append(record)
+
+                counter = counter + 1
 
 
     return filteredRecords
